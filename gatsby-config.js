a05cb5ff671b99699ce633979b711ed768695d9a
Thomas Maximini <tmaximini@gmail.com>
2017-12-24 15:30:29 +0100

    setup for multiple markdown sources

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,8 +8,15 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/src/pages`,
-        name: 'pages'
+        path: `${__dirname}/src/posts`,
+        name: 'posts'
+      }
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `projects`,
+        path: `${__dirname}/src/projects/`
       }
     },
     'gatsby-transformer-remark',
