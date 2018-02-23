a0a26a0d272bd9fc9e4e17772d03012851891079
Thomas Maximini <tmaximini@gmail.com>
2018-02-23 16:15:46 +0100

    404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,25 @@
 import React from 'react'
+import Link from 'gatsby-link'
+import Section from '../components/Shared/Section/Section'
+import doge from './404-doge.png'
 
 const NotFoundPage = () => (
-  <div>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </div>
+  <Section style={{ position: 'relative' }}>
+    <h1>Nothing here.</h1>
+    <p style={{ textAlign: 'center' }}>
+      That's a 404. You just hit a route that doesn&#39;t exist... Sorry about that.
+    </p>
+    <p style={{ textAlign: 'center' }}>
+      <img
+        src={doge}
+        alt="doge 404"
+        style={{ textAlign: 'center', borderRadius: '50%', height: '200px' }}
+      />
+    </p>
+    <p style={{ textAlign: 'center' }}>
+      <Link to="/">Back to Home</Link>
+    </p>
+  </Section>
 )
 
 export default NotFoundPage
