e2c7aee3f61446856993bcfc5555af4625e3e90c
Thomas Maximini <tmaximini@gmail.com>
2019-11-18 20:37:19 +0100

    site verification

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import Helmet from 'react-helmet'
+import React from "react";
+import Helmet from "react-helmet";
 
-import Wrapper from '../Wrapper/Wrapper'
-import Header from '../Header/Header'
-import Main from '../Main/Main'
-import Footer from '../Footer/Footer'
+import Wrapper from "../Wrapper/Wrapper";
+import Header from "../Header/Header";
+import Main from "../Main/Main";
+import Footer from "../Footer/Footer";
 
-import '../../styles/index.scss'
+import "../../styles/index.scss";
 
 const Layout = ({ children }) => (
   <div>
@@ -14,12 +14,18 @@ const Layout = ({ children }) => (
       title="Thomas Maximini: Developer of all things web"
       meta={[
         {
-          name: 'description',
-          content: 'Thomas Maximini is a freelance web developer based in Berlin, Germany'
+          name: "description",
+          content:
+            "Thomas Maximini is a freelance web developer based in Berlin, Germany"
         },
         {
-          name: 'keywords',
-          content: 'web development, software engineering, react.js, javascript, node.js'
+          name: "keywords",
+          content:
+            "web development, software engineering, react.js, javascript, node.js"
+        },
+        {
+          name: "google-site-verification",
+          content: "ZQKLQAaGhOabXPhA_kdXgKHw3hOlbjkGImQAAVD_uG0"
         }
       ]}
     />
@@ -29,6 +35,6 @@ const Layout = ({ children }) => (
       <Footer />
     </Wrapper>
   </div>
-)
+);
 
-export default Layout
+export default Layout;
