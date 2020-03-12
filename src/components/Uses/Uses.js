e7779d2ee8bc3552067f2ab632802742511511c7
Thomas Maximini <tmaximini@gmail.com>
2020-03-12 22:47:19 +0100

    fix uses page

--- src/components/Uses/Uses.js
+++ src/components/Uses/Uses.js
@@ -1,6 +1,5 @@
 import React from "react";
 import styled from "styled-components";
-import { Link } from "gatsby";
 import P from "../Shared/P/P";
 import pic from "./office.jpg";
 
@@ -34,24 +33,24 @@ const Uses = () => (
       <ul>
         <li>
           My favorite editor these days is{" "}
-          <Link to="https://code.visualstudio.com/">VS Code</Link> with the{" "}
+          <a href="https://code.visualstudio.com/">VS Code</a> with the{" "}
           <b>Default Dark+ Theme</b>. I do nearly all my coding in it.
         </li>
         <li>
           From time to time I still go back to{" "}
-          <Link to="https://www.sublimetext.com/3">Sublime Text 3</Link> ot fire
-          up <Link to="https://www.jetbrains.com/webstorm/">Webstorm</Link>.
+          <a href="https://www.sublimetext.com/3">Sublime Text 3</a> ot fire
+          up <a href="https://www.jetbrains.com/webstorm/">Webstorm</a>.
         </li>
         <li>
           My Terminal is the good old{" "}
-          <Link to="https://www.iterm2.com/version3.html">iTerm 3</Link>. I use
+          <a href="https://www.iterm2.com/version3.html">iTerm 3</a>. I use
           it together with{" "}
-          <Link to="https://github.com/ohmyzsh/ohmyzsh">Oh my zsh</Link> and the{" "}
+          <a href="https://github.com/ohmyzsh/ohmyzsh">Oh my zsh</a> and the{" "}
           <b>smt</b> theme.
         </li>
         <li>
           I have a lot of custom stuff and aliases in{" "}
-          <Link to="https://github.com/tmaximini/.dotfiles">dotfiles</Link>.
+          <a href="https://github.com/tmaximini/.dotfiles">my dotfiles</a>.
         </li>
       </ul>
     </p>
@@ -59,43 +58,43 @@ const Uses = () => (
     <ul>
       <li>
         My most important tool these days is{" "}
-        <Link to="https://1password.com/">1Password</Link> which keeps track of
+        <a href="https://1password.com/">1Password</a> which keeps track of
         all my passwords.
       </li>
       <li>
-        I use <Link to="https://todoist.com/app">Todoist</Link> to keep track of
+        I use <a href="https://todoist.com/app">Todoist</a> to keep track of
         my tasks and daily chores.
       </li>
       <li>
         For my writing and taking quick notes I use{" "}
-        <Link to="https://ia.net/writer">iA Writer</Link>.
+        <a href="https://ia.net/writer">iA Writer</a>.
       </li>
       <li>
-        <Link to="https://notion.so">Notion</Link> is another note-taking and
+        <a href="https://notion.so">Notion</a> is another note-taking and
         journaling tool I don't want to live without.
       </li>
       <li>
-        I use <Link to="https://www.spectacleapp.com/">Spectacle</Link> as my
+        I use <a href="https://www.spectacleapp.com/">Spectacle</a> as my
         window manager.
       </li>
       <li>
-        <Link to="https://www.macbartender.com/">Bartender</Link> keeps my OSX
+        <a href="https://www.macbartender.com/">Bartender</a> keeps my OSX
         menu bar clean and organized.
       </li>
       <li>
-        <Link to="https://www.dropbox.com">Dropbox</Link> helps me to sync and
+        <a href="https://www.dropbox.com">Dropbox</a> helps me to sync and
         backup all my important files across devices.
       </li>
       <li>
-        I love <Link to="https://docs.docker.com/docker-for-mac/">Docker</Link>{" "}
+        I love <a href="https://docs.docker.com/docker-for-mac/">Docker</a>{" "}
         because it helps me to keep my dev machine clean.
       </li>
       <li>
-        Sometimes I use <Link to="https://brew.sh/">Homebrew</Link> to install
+        Sometimes I use <a href="https://brew.sh/">Homebrew</a> to install
         missing packages on OSX.
       </li>
       <li>
-        <Link to="https://justgetflux.com/">Flux</Link> improves the quality of
+        <a href="https://justgetflux.com/">Flux</a> improves the quality of
         my sleep when working late on my computer.
       </li>
     </ul>
@@ -103,16 +102,16 @@ const Uses = () => (
     <ul>
       <li>
         My main desk is a{" "}
-        <Link to="https://www.inwerk-bueromoebel.de/buerotische/hoehenverstellbare-schreibtische/steh-sitz-schreibtisch-masterlift2-sw-chrom-bm56947.html">
+        <a href="https://www.inwerk-bueromoebel.de/buerotische/hoehenverstellbare-schreibtische/steh-sitz-schreibtisch-masterlift2-sw-chrom-bm56947.html">
           Masterlift 2
-        </Link>{" "}
+        </a>{" "}
         adjustable standing desk.
       </li>
       <li>
         My office chair is a used{" "}
-        <Link to="https://www.designcabinet.de/herman-miller-aeron-chair-groesse-b-655">
+        <a href="https://www.designcabinet.de/herman-miller-aeron-chair-groesse-b-655">
           Hermann Miller Aeron
-        </Link>{" "}
+        </a>{" "}
         in size B.
       </li>
       <li>
@@ -121,14 +120,14 @@ const Uses = () => (
       </li>
       <li>
         My monitor is the{" "}
-        <Link to="hhttps://www.amazon.com/LG-32UD59-B-32-Inch-LED-Lit-FreeSync/dp/B0748KJY4Q">
+        <a href="hhttps://www.amazon.com/LG-32UD59-B-32-Inch-LED-Lit-FreeSync/dp/B0748KJY4Q">
           LG 32UD59-B
-        </Link>{" "}
+        </a>{" "}
         with a 2560 x 1440 resolution.
       </li>
       <li>
         I use the{" "}
-        <Link to="https://www.therooststand.com/">roost laptop stand</Link> for
+        <a href="https://www.therooststand.com/">roost laptop stand</a> for
         my Laptop at home and when I travel.
       </li>
       <li>I use the Apple magic mouse and magic keyboard.</li>
